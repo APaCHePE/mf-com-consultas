546fcfc28914553e9ee5e22a9d43992b0a286d06
Alex Prado <alexanders.prado@interseguro.com.pe>
2025-05-30 00:06:35 -0500

    Changes view

--- src/types/extramodule.d.ts
+++ src/types/extramodule.d.ts
@@ -1,5 +1,12 @@
 declare module '@test/mf-utils-modules' {
   export function isSidebarMenuItemsVisible(roles: string[]): boolean;
+
+  export function getLayoutConfig(): any;
+  export function updateLayoutConfig(patch: Partial<any>): void;
+  export function updateLayoutState(patch: Partial<any>): void;
+  export function toggleDarkTheme(): void;
+  export function toggleMobileMenu(show: boolean): void;
+  export function getLayoutState(): any;
 }
 
 declare module '@prisma/mf-shared-ui' {
